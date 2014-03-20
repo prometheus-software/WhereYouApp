96f3f26b7c58d785b1f6c8cc468e66b52774fde3
Prometheus Software <klin303@bellsouth.net>
2014-03-19 21:25:54 -0400

    Update MainScreen.java
    Modified tutorial message

--- WhereYouApp/src/com/example/whereyouapp/MainScreen.java
+++ WhereYouApp/src/com/example/whereyouapp/MainScreen.java
@@ -64,10 +64,11 @@ public class MainScreen extends Activity {
 		new AlertDialog.Builder(this)
 	    .setTitle("How to use Where You App?")
 	    .setMessage("1. Click on the Add Route button in the middle of the main screen." +
-	    		"\n\n2. On the AddRouteScreen, put in the following information: route name, address (by picking an address on the dialog box results from the SetAddressScreen), target radius, phone number, and text message." + 
-	    		"\n\n3. Hit the save button and the route will be saved." +
-	    		"\n\n 4. Your contact will be notified as soon as your GPS coordinates are within the target radius of the destination GPS coordinates." + 
-	    		"\n\n 5. Drive/commute safely!\n")
+	    		"\n\n2. On the AddRouteScreen, put in the following information: route name, address (by picking an address on the dialog box results from the SetAddressScreen), target radius, phone numbers (you can also use the Contacts button for Contacts integration), and text message." + 
+	    		"\n\n3. Hit the save button and the route will be saved in the SavedRoutesScreen (under the My Routes button)." +
+	    		"\n\n4. Your contact will be notified as soon as your GPS coordinates are within the target radius of the destination GPS coordinates." +
+	    		"\n\n5. Set up your emergency contact numbers for a low battery level in the Settings Screen (via the Settings button)" +
+	    		"\n\n6. Drive/commute safely!\n")
 	    .setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) { 
 	        	
