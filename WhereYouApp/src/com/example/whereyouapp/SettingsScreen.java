6a6b5ff0610c5bf05a8baba4f5d7e00b448e9019
Prometheus Software <klin303@bellsouth.net>
2014-03-26 18:06:21 -0400

    Update SettingsScreen.java
    Got rid of EditTexts for emergency contact numbers (will be used within a route)

--- WhereYouApp/src/com/example/whereyouapp/SettingsScreen.java
+++ WhereYouApp/src/com/example/whereyouapp/SettingsScreen.java
@@ -58,6 +58,7 @@ public class SettingsScreen extends Activity {
 		if(setdbHandle.containsValue()) {
 			int savedBatteryLevel = setdbHandle.getSavedBatteryLevel();
 			if(savedBatteryLevel > -1)
+			{
 				if(setdbHandle.getSavedBatteryLevel()==5)
 					spinner1.setSelection(1);
 				if(setdbHandle.getSavedBatteryLevel()==10)
@@ -68,28 +69,8 @@ public class SettingsScreen extends Activity {
 					spinner1.setSelection(4);
 				if(setdbHandle.getSavedBatteryLevel()==25)
 					spinner1.setSelection(5);
+			}
 		}
-		EditText editText = (EditText) findViewById(R.id.enter_contact1);
-		editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		editText = (EditText) findViewById(R.id.enter_contact2);
-		editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		editText = (EditText) findViewById(R.id.enter_contact3);
-		editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		editText = (EditText) findViewById(R.id.enter_contact4);
-		editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		editText = (EditText) findViewById(R.id.enter_contact5);
-		editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		timesClicked = 0;
-		((Button)findViewById(R.id.contact_list_button)).setOnClickListener( new OnClickListener() {
-	        @Override
-	        public void onClick(View v) {
-	        	timesClicked++;
-	        	Toast.makeText(v.getContext(), "Times clicked: " + timesClicked, Toast.LENGTH_LONG).show();
-	    	    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-	            intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-	            startActivityForResult(intent, 1); 	            
-	        } 
-	    });
 	}
 
 	@Override
@@ -102,142 +83,9 @@ public class SettingsScreen extends Activity {
 	{
 		spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener ());
 	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (data != null) {
-	        Uri uri = data.getData();
-
-	        if (uri != null) {
-	            Cursor c = null;
-	            try {
-	                c = getContentResolver().query(uri, new String[]{ 
-	                            ContactsContract.CommonDataKinds.Phone.NUMBER,  
-	                            ContactsContract.CommonDataKinds.Phone.TYPE },
-	                        null, null, null);
-
-	                if (c != null && c.moveToFirst()) {
-	                    String number = c.getString(0);
-	                    int type = c.getInt(1);
-	                    showSelectedNumber(type, number);
-	                }
-	            } finally {
-	                if (c != null) {
-	                    c.close();
-	                }
-	            }
-	        }
-	    }
-	}
-	public void showSelectedNumber(int type, String number) {
-	    Toast.makeText(this, type + ": " + number, Toast.LENGTH_LONG).show();
-	    if (timesClicked % 5 == 1)
-	    {
-	    	EditText editText = (EditText) findViewById(R.id.enter_contact1);
-	    	editText.setText(number, TextView.BufferType.EDITABLE);
-	    }
-	    else if (timesClicked % 5 == 2)
-	    {
-	    	EditText editText = (EditText) findViewById(R.id.enter_contact2);
-	    	editText.setText(number, TextView.BufferType.EDITABLE);
-	    }
-	    else if (timesClicked % 5 == 3)
-	    {
-	    	EditText editText = (EditText) findViewById(R.id.enter_contact3);
-	    	editText.setText(number, TextView.BufferType.EDITABLE);
-	    }
-	    else if (timesClicked % 5 == 4)
-	    {
-	    	EditText editText = (EditText) findViewById(R.id.enter_contact4);
-	    	editText.setText(number, TextView.BufferType.EDITABLE);
-	    }
-	    else
-	    {
-	    	EditText editText = (EditText) findViewById(R.id.enter_contact5);
-	    	editText.setText(number, TextView.BufferType.EDITABLE);
-	    }
-	}
 	public void saveMySettings (View view)
 	{
 		Intent intent = new Intent(this, MainScreen.class);
-		EditText editText = (EditText) findViewById(R.id.enter_contact1);
-		timesClicked = 0;
-		String [] phoneNumbers = new String [5];
-		String phoneNum = editText.getText().toString();
-		String delims = "()-+";
-		String [] tokens = phoneNum.split(delims);
-		for (int i = 0; i < tokens.length; i ++)
-		{
-			phoneNumbers [0] += tokens [i];
-		}
-		boolean error = false;
-		editText = (EditText) findViewById(R.id.enter_contact2);
-		String phoneNum1 = editText.getText().toString();
-		tokens = phoneNum1.split(delims);
-		for (int i = 0; i < tokens.length; i ++)
-		{
-			phoneNumbers [1] += tokens [i];
-		}
-		editText = (EditText) findViewById(R.id.enter_contact3);
-		String phoneNum2 = editText.getText().toString();
-		tokens = phoneNum2.split(delims);
-		for (int i = 0; i < tokens.length; i ++)
-		{
-			phoneNumbers [2] += tokens [i];
-		}
-		editText = (EditText) findViewById(R.id.enter_contact4);
-		String phoneNum3 = editText.getText().toString();
-		tokens = phoneNum3.split(delims);
-		for (int i = 0; i < tokens.length; i ++)
-		{
-			phoneNumbers [3] += tokens [i];
-		}
-		editText = (EditText) findViewById(R.id.enter_contact5);
-		String phoneNum4 = editText.getText().toString();
-		tokens = phoneNum4.split(delims);
-		for (int i = 0; i < tokens.length; i ++)
-		{
-			phoneNumbers [4] += tokens [i];
-		}
-//		try
-//		{
-//			int part1 = Integer.parseInt(phoneNumbers [0].substring(4));
-//			Log.d(TAG, "" + part1);
-//		}catch(NumberFormatException e)
-//		{
-//			error = true;
-//		}
-//		try
-//		{
-//			int part2 = Integer.parseInt(phoneNumbers [1].substring(4));
-//			Log.d(TAG, "" + part2);
-//		}catch(NumberFormatException e)
-//		{
-//			error = true;
-//		}
-//		try
-//		{
-//			int part3 = Integer.parseInt(phoneNumbers [2].substring(4));
-//			Log.d(TAG, "" + part3);
-//		}catch(NumberFormatException e)
-//		{
-//			error = true;
-//		}
-//		try
-//		{
-//			int part4 = Integer.parseInt(phoneNumbers [3].substring(4));
-//			Log.d(TAG, "" + part4);
-//		}catch(NumberFormatException e)
-//		{
-//			error = true;
-//		}
-//		try
-//		{
-//			int part5 = Integer.parseInt(phoneNumbers [4].substring(4));
-//			Log.d(TAG, "" + part5);
-//		}catch(NumberFormatException e)
-//		{
-//			error = true;
-//		}
 		try
 		{
 			batteryLevel = Integer.parseInt(String.valueOf(spinner1.getSelectedItem()));
@@ -246,68 +94,13 @@ public class SettingsScreen extends Activity {
 		{
 			Toast.makeText(this, "Error with battery level", Toast.LENGTH_LONG).show();
 		}
-		if (error)
-		{
-			Toast.makeText(this, "Error with at least phone number", Toast.LENGTH_LONG).show();
-			intent = new Intent (this, SettingsScreen.class);
-		}
-		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-		mBuilder.setAutoCancel(true);
-		mBuilder.setSmallIcon(R.drawable.ic_launcher);
-		mBuilder.setContentTitle("Your battery level is below " + batteryLevel + "%.");
-		
 		setdbHandle.deleteBatterySetting();
 		setdbHandle.insertBatteryLevel(batteryLevel);
-		
-		if (phoneNumbers [0].equals("null"))
-		{
-			mBuilder.setContentText("Your phone is about to die. Are you sure you don't want to let someone know your location?");
-		}
-		else if (phoneNumbers [1].equals("null"))
-		{
-			mBuilder.setContentText("Your location has been sent to " + phoneNumbers [0].substring(4) + " before your phone dies.");
-		}
-		else if (phoneNumbers [2].equals ("null"))
-		{
-			mBuilder.setContentText("Your location has been sent to " + phoneNumbers [0].substring(4) + " and " + phoneNumbers [1].substring(4) + " before your phone dies.");
-		}
-		else if (phoneNumbers [3].equals ("null"))
-		{
-			mBuilder.setContentText("Your location has been sent to " + phoneNumbers [0].substring(4) + " , " + phoneNumbers [1].substring(4) + " , " + " and " + phoneNumbers [2].substring(4) +  " before your phone dies.");
-		}
-		else if (phoneNumbers [4].equals("null"))
-		{
-			mBuilder.setContentText("Your location has been sent to " + phoneNumbers [0].substring(4) + " , " + phoneNumbers [1].substring(4) + " , " + phoneNumbers [2].substring(4) + " and " + phoneNumbers [3].substring(4) +  " before your phone dies.");
-		}
-		else
-		{
-			mBuilder.setContentText("Your location has been sent to " + phoneNumbers [0].substring(4) + " , " + phoneNumbers [1].substring(4) + " , " + phoneNumbers [2].substring(4) + " , " + phoneNumbers [3].substring(4) + " and " + phoneNumbers[4].substring(4) + " before your phone dies.");
-		}
-		Intent resultIntent = new Intent (this, MainScreen.class);
-		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-		stackBuilder.addParentStack(MainScreen.class);
-		stackBuilder.addNextIntent(resultIntent);
-		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
-		mBuilder.setContentIntent(resultPendingIntent);
-		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		mBuilder.setDefaults(Notification.DEFAULT_ALL);
-		mNotificationManager.notify(1, mBuilder.build());
 		startActivity(intent);
 	}
 	public void cancelSettings (View view)
 	{
 		spinner1.setSelection(0);
-		timesClicked = 0;
-		EditText editText = (EditText) findViewById(R.id.enter_contact1);
-		editText.setText("", TextView.BufferType.EDITABLE);
-		editText = (EditText) findViewById(R.id.enter_contact2);
-		editText.setText("", TextView.BufferType.EDITABLE);
-		editText = (EditText) findViewById(R.id.enter_contact3);
-		editText.setText("", TextView.BufferType.EDITABLE);
-		editText = (EditText) findViewById(R.id.enter_contact4);
-		editText.setText("", TextView.BufferType.EDITABLE);
-		editText = (EditText) findViewById(R.id.enter_contact5);
-		editText.setText("", TextView.BufferType.EDITABLE);
 		Intent intent = new Intent (this, MainScreen.class);
 		startActivity(intent);
 	}
