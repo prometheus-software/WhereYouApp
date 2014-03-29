e802cd262a2576d41a3967ca1298e8ae1787cb5b
Prometheus Software <klin303@bellsouth.net>
2014-03-29 14:20:06 -0400

    Update MainScreen.java
    More accurate Tutorial message

--- WhereYouApp/src/com/whereyouapp/ufl/edu/MainScreen.java
+++ WhereYouApp/src/com/whereyouapp/ufl/edu/MainScreen.java
@@ -60,10 +60,10 @@ public class MainScreen extends Activity {
 		new AlertDialog.Builder(this)
 	    .setTitle("How to use Where You App?")
 	    .setMessage("1. Click on the Add Route button in the middle of the main screen." +
-	    		"\n\n2. On the AddRouteScreen, put in the following information: route name, address (by picking an address on the dialog box results from the SetAddressScreen), target radius, phone numbers (you can also use the Contacts button for Contacts integration), and text message." + 
-	    		"\n\n3. Hit the save button and the route will be saved in the SavedRoutesScreen (under the My Routes button)." +
+	    		"\n\n2. On the AddRouteScreen, put in the following information: route name, address (by picking an address on the dialog box results from the SetAddressScreen or clicking on the map), target radius (in miles or kilometers), phone numbers (you can also use the Contacts buttons for Contacts integration), and text message." + 
+	    		"\n\n3. Hit the save button and the route will be saved to the main screen. By clicking on a saved route on the main screen, you can toggle it active or delete it." +
 	    		"\n\n4. Your contact will be notified as soon as your GPS coordinates are within the target radius of the destination GPS coordinates." +
-	    		"\n\n5. Set up your emergency contact numbers for a low battery level in the Settings Screen (via the Settings button)" +
+	    		"\n\n5. Set up your threshold battery level in the Settings Screen (via the Settings button). When you're on a route and your battery level falls below that battery level, the contacts for the route will be notified of your location before your phone dies." +
 	    		"\n\n6. Drive/commute safely!\n")
 	    .setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) { 
@@ -73,3 +73,4 @@ public class MainScreen extends Activity {
 	     .show().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 	}
 }
+
