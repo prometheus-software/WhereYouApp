96f6872893b497b8247632894aea5f3215b14b8f
Prometheus Software <klin303@bellsouth.net>
2014-04-09 14:45:07 -0400

    Update RouteDBHelper.java
    Should no longer crash

--- WhereYouApp/src/com/whereyouapp/ufl/edu/RouteDBHelper.java
+++ WhereYouApp/src/com/whereyouapp/ufl/edu/RouteDBHelper.java
@@ -75,5 +75,11 @@ public class RouteDBHelper extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 		onCreate(db);
 	}
+	
+	@Override
+	public void onDowngrade(SQLiteDatabase db, int arg1, int arg2)
+	{
+		onUpgrade(db, arg1, arg2);
+	}
 
 }
