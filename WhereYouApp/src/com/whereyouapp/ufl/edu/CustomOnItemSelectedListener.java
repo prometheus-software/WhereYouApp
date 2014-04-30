7f1d379cbd3da879db24727b461e810ec39a60bb
Prometheus Software <klin303@bellsouth.net>
2014-04-30 15:15:58 -0400

    Update CustomOnItemSelectedListener.java
    Toast message gone

--- WhereYouApp/src/com/whereyouapp/ufl/edu/CustomOnItemSelectedListener.java
+++ WhereYouApp/src/com/whereyouapp/ufl/edu/CustomOnItemSelectedListener.java
@@ -5,7 +5,7 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 public class CustomOnItemSelectedListener implements OnItemSelectedListener {
 	public void onItemSelected (AdapterView<?> parent, View view, int pos, long id){
-		Toast.makeText(parent.getContext(), "You have selected: " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+		//Toast.makeText(parent.getContext(), "You have selected: " + parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
 	}
 	@Override
 	public void onNothingSelected (AdapterView <?> arg0)
