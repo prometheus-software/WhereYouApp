89f60244f439fc73bf79508940bd75f87e749ae5
Prometheus Software <klin303@bellsouth.net>
2014-04-30 15:56:38 -0400

    Update SavedRoutesScreen.java
    A fix!

--- WhereYouApp/src/com/whereyouapp/ufl/edu/SavedRoutesScreen.java
+++ WhereYouApp/src/com/whereyouapp/ufl/edu/SavedRoutesScreen.java
@@ -30,8 +30,8 @@ public class SavedRoutesScreen extends Activity{
 	static int currentRouteIndex;
 	public static RouteDataSource dbHandle;
 	public static SettingsDataSource setdbHandle;
-	public static AddressDataSource addressdbHandle;
 	public static boolean isActive;
+	public static AddressDataSource addressdbHandle;
 	@SuppressLint("NewApi")
 	//@Override
 	/*public void onCreate(Bundle savedInstanceState) {
@@ -150,7 +150,9 @@ public class SavedRoutesScreen extends Activity{
 						//
 						//
 						//Starts the AddRouteScreen and passes the position of the route in the database
+						//WTF is position? ID maybe? Also, integer != LatLng
 						myIntent.putExtra("position", (Integer)v.getTag());
+						//myIntent.putExtra(name, value);
 						startActivity(myIntent);						
 					}
 				});
@@ -203,6 +205,68 @@ public class SavedRoutesScreen extends Activity{
 	{
 		//Update routes to reflect any changes in the database
 		routes = dbHandle.getAllRoutes();
+		ListView list = new ListView(this);
+		//Build the layout from SQLite database query
+				if(routes == null)
+				{
+					System.out.println("NOTHING");
+				}
+				else {
+					
+				String[] items = new String[routes.size()];
+				for(int i = 0; i < items.length; i++) {
+					items[i] = routes.get(i).getName();
+				}
+		        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row_view, R.id.text, items) {
+		            @Override
+		            public View getView(int position, View convertView, ViewGroup parent) {
+		            	View row =  super.getView(position, convertView, parent);
+		                row.setBackgroundResource(R.drawable.actionbar_bg);
+		                View routeName = row.findViewById(R.id.text);
+		                routeName.setTag(position);
+		                routeName.setOnClickListener(new AdapterView.OnClickListener() {              
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								Intent myIntent = new Intent(getApplicationContext(), EditRouteScreen.class);
+								//
+								//
+								//Starts the AddRouteScreen and passes the position of the route in the database
+								//WTF is position? ID maybe? Also, integer != LatLng
+								myIntent.putExtra("position", (Integer)v.getTag());
+								//myIntent.putExtra(name, value);
+								startActivity(myIntent);						
+							}
+						});
+		                ImageButton imgb =  (ImageButton)row.findViewById(R.id.on);
+		                View right = row.findViewById(R.id.on);               
+		               if (routes.get(position).getIsActive() == 1) {
+		            	   imgb.setImageResource(R.drawable.on);
+		               }
+		               else {
+		            	   imgb.setImageResource(R.drawable.off);
+		               }
+		                right.setTag(position);
+		                right.setOnLongClickListener(new AdapterView.OnLongClickListener() {
+							@Override
+							public boolean onLongClick(View v) {
+								// TODO Auto-generated method stub
+								SavedRoutesScreen.showRouteOptions((Integer) v.getTag());
+								return true;
+							}
+						});                
+		                right.setOnClickListener(new AdapterView.OnClickListener() {
+							@Override
+							public void onClick(View v) {
+								SavedRoutesScreen.toggleActive((Integer) v.getTag());
+							}
+		                });                    
+		                return row;               
+		            }
+		        };       
+		        Controller.setServiceAlarm(this, true);     
+		        list.setAdapter(adapter);
+				}
 		isActive = true;
 		super.onResume();
 	}
@@ -210,6 +274,7 @@ public class SavedRoutesScreen extends Activity{
 	public static void showRouteOptions(int i)
 	{
 		currentRouteIndex = i;
+		routes = dbHandle.getAllRoutes();
 		final Route selectedRoute = routes.get(i);
 		String message = "Name: " + selectedRoute.getName();
 		message += "\n";
@@ -246,9 +311,23 @@ public class SavedRoutesScreen extends Activity{
 		message += "\n";
 		ArrayList<String> time = selectedRoute.getTime();
 		message += "Time: ";
-		message += time.get(0);
+		if (time.get(0).equals("0") || time.get(0).equals("1") || time.get(0).equals("2") || time.get(0).equals("3") || time.get(0).equals("4") || time.get(0).equals("5") || time.get(0).equals("6") || time.get(0).equals("7") || time.get(0).equals("8") || time.get(0).equals("9"))
+		{
+			message += "0" + time.get(0);
+		}
+		else
+		{
+			message += time.get(0);
+		}
 		message += ":";
-		message += time.get(1);
+		if (time.get(1).equals("0") || time.get(1).equals("1") || time.get(1).equals("2") || time.get(1).equals("3") || time.get(1).equals("4") || time.get(1).equals("5") || time.get(1).equals("6") || time.get(1).equals("7") || time.get(1).equals("8") || time.get(1).equals("9"))
+		{
+			message += "0" + time.get(1);
+		}
+		else
+		{
+			message += time.get(1);
+		}
 		message += "\n";
 		message += "Days: ";
 		ArrayList<Integer> days = selectedRoute.getDays();
@@ -317,7 +396,7 @@ public class SavedRoutesScreen extends Activity{
 		        }
 		  })
 		  
-		  .show().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);;
+		  .show().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 	}
 
 	public static void deleteRoute(int i)
@@ -373,9 +452,9 @@ public class SavedRoutesScreen extends Activity{
 	    .setMessage("1. Click on the Add Route button at the top of the main screen." +
 	    		"\n\n2. On the AddRouteScreen, put in the following information: route name, address (by picking an address on the dialog box results from the SetAddressScreen, clicking on the map (in which case you click on the marker to go back to the AddRouteScreen), or picking an old address), target radius (in miles or kilometers), phone numbers (you can also use the Contacts buttons for Contacts integration), and text message." +
 	    		"\n\n3. You can also select a schedule for your route by going to the Commute option in the action bar. You can choose a time and which days of the week on which you want your route to run, so that the controller will automatically launch your route on those days and times." +
-	    		"\n\n4. Hit the save button and the route will be saved to the main screen. By clicking on a saved route on the main screen, you can edit its fields (name must be changed), while holding down on the power switch allows you to delete a route or view its address. Hitting the power switch will toggle the route active." +
+	    		"\n\n4. Hit the save button and the route will be saved to the main screen. By clicking on a saved route on the main screen, you can edit its fields, while holding down on the power switch allows you to delete a route or view its address. Hitting the power switch will toggle the route active." +
 	    		"\n\n5. Your contact will be notified as soon as your GPS coordinates are within the target radius of the destination GPS coordinates." +
-	    		"\n\n6. Set up your threshold battery level in the Settings Screen (via the Settings button). When you're on a route and your battery level falls below that battery level, the contacts for the route will be notified of your location before your phone dies." +
+	    		"\n\n6. Set up your threshold battery level in the Settings Screen (via the Settings button). When you're on a route and your battery level falls below that battery level, the contacts for the route will be notified of your location before your phone dies (assuming the controller is on)." +
 	    		"\n\n7. Drive/commute safely!\n")
 	    .setPositiveButton("Got it!", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) { 
@@ -390,4 +469,3 @@ public class SavedRoutesScreen extends Activity{
 		return;
 	}
 }
-
