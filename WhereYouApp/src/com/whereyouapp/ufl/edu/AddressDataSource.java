bb9773c2beeea2cd2c7c927e290abc97753e504b
Prometheus Software <ufbridgeclub@gmail.com>
2014-05-07 16:32:24 -0400

    Update AddressDataSource.java
    Got rid of all unnecessary code

--- WhereYouApp/src/com/whereyouapp/ufl/edu/AddressDataSource.java
+++ WhereYouApp/src/com/whereyouapp/ufl/edu/AddressDataSource.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
